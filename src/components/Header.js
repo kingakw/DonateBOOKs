9270fcfad5fc256e11f76ccc6fd9c97c3ca1a2b6
Kinga <kingakw@yahoo.com>
2022-02-16 16:42:29 +0100

    logina & register

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {NavLink} from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import { Link } from 'react-scroll';
 
 const Header = () => {
-    return (
+
+    const [isActive, setActive] = useState(false);
+
+    const toggleClass = () => {
+        setActive(!isActive);
+    };
+
+        return (
         <div className="header__container">
             <nav className="header__nav">
-                <NavLink className="header__nav__el" to="/Login" element={<Login/>}>
+                <NavLink className="header__nav__el"  to="/Login" element={<Login/> } onClick={toggleClass} >
                     Zaloguj
                 </NavLink>
-                <NavLink className="header__nav__el" to="/Register" element={<Register/>}>
+                <NavLink className="header__nav__el" to="/Register" element={<Register/>} onClick={toggleClass}>
                     Załóż konto
                 </NavLink>
             </nav>
 
             <ul className="header__menu">
-                <li> Start  </li>
+                <li>
+                    <NavLink className="header__menu__el"  to="/" element={<Header/>}> Start
+                    </NavLink>
+                    </li>
                 <li> <Link to ="fourSteps" smooth={true} > O co chodzi? </Link> </li>
                 <li> <Link to ="about" smooth={true} > O nas </Link> </li>
                 <li> <Link to ="help" smooth={true} > Fundacja i organizacje </Link> </li>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Decoration from "../assets/Decoration.svg";
+import {NavLink} from "react-router-dom";
+import Login from "./Login";
 
 
 const Register = () => {
     return (
-        <main>
-            Register
-        </main>
+        <div className="regContainer">
+            <p className="regContainer__text"> Załóż konto </p>
+            <div className="regContainer__decor">
+                <img src={Decoration} alt="decoration"/>
+            </div>
+
+            <div className="regContainer__grey">
+                <form onSubmit={''}>
+                    <label className="regContainer__email">
+                        Email
+                        <input type="text" name="email"/>
+                    </label>
+                    <label className="regContainer__password">
+                        Hasło
+                        <input type="text" name="password"/>
+                    </label>
+                    <label className="regContainer__password">
+                        Powtórz hasło
+                        <input type="text" name="password"/>
+                    </label>
+                </form>
+            </div>
+
+            <div className="regContainer__nav">
+                <NavLink className="regContainer__nav__el" to="/Login" element={<Login/>}>
+                    Zaloguj się
+                </NavLink>
+                <div className="regContainer__btn">
+                    <input type="submit" value="Załóż konto"/>
+                </div>
+            </div>
+
+        </div>
     );
 };
 export default Register;
